66306db3fcbda663167036cc7359dd7d508131a1
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-03-21 18:42:41 -0400

    Assignment 15 complete

--- ios-exercises/BlocExercises/BlocExercises/NumberHandler.m
+++ ios-exercises/BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,41 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
-}
+    
+    int b = [number intValue];
+    b = b * 2;
+    number = [NSNumber numberWithInt:b];
+    return number;
 
+}
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+
+    
+    NSMutableArray *numbersArray = [[NSMutableArray alloc] init];
+    
+    if (number > otherNumber) {
+        NSInteger temp = number;
+        number = otherNumber;
+        otherNumber = temp;
+    }
+    
+    for (NSInteger i = number; i <= otherNumber; i++) {
+        NSNumber *numberObject = [[NSNumber alloc] init];
+        numberObject = [NSNumber numberWithInteger: i];
+        [numbersArray addObject: numberObject];
+    
+        
+    }
+    return numbersArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    
+    NSArray *sortedArray = [arrayOfNumbers sortedArrayUsingSelector:@selector(compare:)];
+    NSInteger lowestNumber = [sortedArray[0] floatValue];
+    
+    return lowestNumber;
 }
 
 @end
+
